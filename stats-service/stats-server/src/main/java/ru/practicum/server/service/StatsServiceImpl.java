edd0f2f80158a178e895bc472a7260f662e86de5
wcobq-home <tolmachev.e.s@yandex.ru>
2024-03-01 01:20:34 +0300

    Добавлен функционал

--- stats-service/stats-server/src/main/java/ru/practicum/server/service/StatsServiceImpl.java
+++ stats-service/stats-server/src/main/java/ru/practicum/server/service/StatsServiceImpl.java
@@ -39,34 +39,4 @@ public class StatsServiceImpl implements StatsService {
                 .collect(Collectors.toList());
 
     }
-
-
-    private List<ViewStats> toViewStatsList(List<Object[]> objects) {
-        Map<String, ViewStats> allStats = new HashMap<>();
-        for (Object[] object : objects) {
-            String key = object[0].toString() + object[1].toString();
-            if (allStats.containsKey(key)) {
-                allStats.get(key).getHits().put(object[2].toString(), Integer.parseInt(object[3].toString()));
-            } else {
-                Map<String, Integer> hits = new HashMap<>() {{
-                    put("hi", 1);
-                }};
-                hits.put(object[2].toString(), Integer.parseInt(object[3].toString()));
-            }
-        }
-        return new ArrayList<>(allStats.values());
-    }
-
-    private List<ViewStatsDto> toViewStatsDtoList(List<Object[]> objects, boolean unique) {
-        List<ViewStats> viewStats = toViewStatsList(objects);
-        return viewStats.stream()
-                .map(view -> StatMapper.STAT_MAPPER.viewToDto(view, unique))
-                .sorted(new Comparator<ViewStatsDto>() {
-                    @Override
-                    public int compare(ViewStatsDto o1, ViewStatsDto o2) {
-                        return o2.getHits() - o1.getHits();
-                    }
-                })
-                .collect(Collectors.toList());
-    }
 }
