a3ca6bd3c454aa603d94fb90a685e622f9d86e91
wcobq-home <tolmachev.e.s@yandex.ru>
2024-03-01 00:53:23 +0300

    Добавлен функционал

--- stats-service/stats-server/src/main/java/ru/practicum/server/service/StatsServiceImpl.java
+++ stats-service/stats-server/src/main/java/ru/practicum/server/service/StatsServiceImpl.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Service;
 import ru.practicum.client.models.HitDto;
 import ru.practicum.client.models.ViewStatsDto;
 import ru.practicum.server.mappers.StatMapper;
-import ru.practicum.server.models.ViewStats;
 import ru.practicum.server.repository.HitEntity;
 import ru.practicum.server.repository.StatRepository;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -38,35 +38,5 @@ public class StatsServiceImpl implements StatsService {
                 })
                 .collect(Collectors.toList());
 
-    }
-
-
-    private List<ViewStats> toViewStatsList(List<Object[]> objects) {
-        Map<String, ViewStats> allStats = new HashMap<>();
-        for (Object[] object : objects) {
-            String key = object[0].toString() + object[1].toString();
-            if (allStats.containsKey(key)) {
-                allStats.get(key).getHits().put(object[2].toString(), Integer.parseInt(object[3].toString()));
-            } else {
-                Map<String, Integer> hits = new HashMap<>() {{
-                    put("hi", 1);
-                }};
-                hits.put(object[2].toString(), Integer.parseInt(object[3].toString()));
-            }
-        }
-        return new ArrayList<>(allStats.values());
-    }
-
-    private List<ViewStatsDto> toViewStatsDtoList(List<Object[]> objects, boolean unique) {
-        List<ViewStats> viewStats = toViewStatsList(objects);
-        return viewStats.stream()
-                .map(view -> StatMapper.STAT_MAPPER.viewToDto(view, unique))
-                .sorted(new Comparator<ViewStatsDto>() {
-                    @Override
-                    public int compare(ViewStatsDto o1, ViewStatsDto o2) {
-                        return o2.getHits() - o1.getHits();
-                    }
-                })
-                .collect(Collectors.toList());
     }
 }
