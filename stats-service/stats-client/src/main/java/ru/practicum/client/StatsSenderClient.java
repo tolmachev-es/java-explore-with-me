1e24fbdb38cd4aab6e5408151c50d7b493733d44
wcobq-home <tolmachev.e.s@yandex.ru>
2024-03-31 20:17:43 +0300

    Добавлены исправления

--- stats-service/stats-client/src/main/java/ru/practicum/client/StatsSenderClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/client/StatsSenderClient.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
